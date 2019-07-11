05d70a1760026acd20f89dfa64430f61a1f16f70
AlexMeraTec <38839954+AlexMeraTec@users.noreply.github.com>
2019-07-10 23:02:10 -0400

    middleware

--- pape/settings.py
+++ pape/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'pape.middleware.ProfileCompletionMiddleware',
 ]
 
 ROOT_URLCONF = 'pape.urls'
--- users/views.py
+++ users/views.py
@@ -1,8 +1,50 @@
 """Users views"""
 #import de Django
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+#exception
+from django.db.utils import IntegrityError
+
+from django.contrib.auth.models import User
+from users.models import Profile
+
+# Forms
+from users.forms import ProfileForm
+
+
+#vista de actualizacion de usuario
+def update_profile(request):
+    """Update a user's profile view."""
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+
+    else:
+        form = ProfileForm()
+
+    return render(
+        request=request,
+        template_name='users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            'form': form
+        }
+    )
+
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
@@ -16,6 +58,32 @@ def login_view(request):
             return render(request,'users/login.html',{'error':'Usuario y Contraseña incorrectos'})
     return render(request,'users/login.html')
 
+def signup_view(request):
+    """Sign up view."""
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Las contraseñas no coinciden'})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Este usuario ya esta en uso'})
+        
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
 @login_required
 def logout_view(request):
     """Logout a user."""
